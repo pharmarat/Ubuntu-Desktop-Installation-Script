730bdd5954f99d0a080ce85494c2bb169794845f
Alex <92735950+pharmarat@users.noreply.github.com>
2023-09-06 00:06:14 +0200

    changes
    removed progress bar
    added colors
    added python installation

--- install.sh
+++ install.sh
@@ -1,53 +1,38 @@
 #!/bin/bash
 
+# Color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 # Check if running as root
 if [ "$EUID" -ne 0 ]; then
-  echo "Error: This script must be run as root. Please use sudo or run as root user."
+  echo -e "${RED}Error: This script must be run as root. Please use sudo or run as root user.${NC}"
   exit 1
 fi
 
-# Function to display progress bar
-function show_progress() {
-  local total=$1
-  local current=$2
-  local width=50
-  local percent=$((current * 100 / total))
-  local completed=$((width * current / total))
-  local remaining=$((width - completed))
-  printf "["
-  printf "%*s" "$completed" | tr ' ' '='
-  printf "%*s" "$remaining" | tr ' ' ' '
-  printf "] %d%%\r" "$percent"
-}
-
 # Update and upgrade system
-echo "Updating system..."
+echo -e "${YELLOW}Updating system...${NC}"
 sudo apt-get update && sudo apt-get upgrade -yy
 
+# Install Python and pip
+echo -e "${YELLOW}Installing Python and pip...${NC}"
+sudo apt-get install python3 python3-pip -yy
+
 # Install Ubuntu Desktop
-echo "Installing Ubuntu Desktop..."
+echo -e "${YELLOW}Installing Ubuntu Desktop...${NC}"
 sudo apt install ubuntu-desktop -yy
 
 # Install Firefox
-echo "Installing Firefox..."
+echo -e "${YELLOW}Installing Firefox...${NC}"
 sudo snap install firefox -yy
 
 # Download and install NoMachine
-echo "Downloading NoMachine..."
+echo -e "${YELLOW}Downloading NoMachine...${NC}"
 wget https://download.nomachine.com/download/8.5/Linux/nomachine_8.5.3_1_amd64.deb
 
-echo "Installing NoMachine..."
+echo -e "${YELLOW}Installing NoMachine...${NC}"
 sudo dpkg -i nomachine_8.5.3_1_amd64.deb
 
-# Display progress bar
-total_steps=5
-current_step=0
-
-while [ $current_step -lt $total_steps ]; do
-  current_step=$((current_step + 1))
-  show_progress $total_steps $current_step
-  sleep 1
-done
-
-echo
-echo "Installation completed!"
+echo -e "${GREEN}Installation completed!${NC}"
